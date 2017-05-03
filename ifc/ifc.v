05ae6705ca737e6e770d5e3bae9dd0725364c5e4
Samuel Gruetter <samuel.gruetter@epfl.ch>
2017-05-03 13:23:47 +1000

    renaming

--- ifc/ifc.v
+++ ifc/ifc.v
@@ -1,3 +1,5 @@
+(* In this file, P, N, A are separate, rather than a triple *)
+
 Require Export compcert.cfrontend.Clight.
 Require Export veric.Clight_new.
 Require Export examples.if1.
@@ -27,8 +29,8 @@ Definition state_pred := corestate -> mem -> Prop.
 Definition pre_assert := environ -> mpred.
 (* "ret_assert := exitkind -> option val -> environ -> mpred" is already defined by VST *)
 
-Definition ifc_pre := (pre_assert * stack_clsf * heap_clsf)%type.
-Definition ifc_post := (ret_assert * stack_clsf * heap_clsf)%type.
+Definition ifc_pre(A: Type) := ((A -> pre_assert) * (A -> stack_clsf) * (A -> heap_clsf))%type.
+Definition ifc_post(A: Type) := ((A -> ret_assert) * (A -> stack_clsf) * (A -> heap_clsf))%type.
 
 (* TODO get rid of these *)
 Parameter VST_pre_to_state_pred : pre_assert -> state_pred.
@@ -54,7 +56,7 @@ Definition stack_lo_equiv(s1 s2: corestate)(N1 N2: stack_clsf): Prop :=
   | _, _ => False
   end.
 
-Definition heap_access(m: mem)(l: heap_loc): memval := 
+Definition heap_access(m: mem)(l: heap_loc): memval :=
   let (b, i) := l in (ZMap.get i (PMap.get b (Mem.mem_contents m))).
 
 Definition heap_lo_equiv(m1 m2: mem)(A1 A2: heap_clsf): Prop :=
@@ -76,7 +78,7 @@ Definition simple_ifc {A : Type} (Delta: tycontext)
    star ge s2 m2 s2' m2' ->
    stack_lo_equiv s1' s2' (postN x1) (postN x2) /\ heap_lo_equiv m1 m2 (postA x1) (postA x2).
 
-Definition ifc_core {A: Type} (Delta: tycontext) 
+Definition ifc_core {A: Type} (Delta: tycontext)
   (preP: A -> pre_assert) (preN: A -> stack_clsf) (preA: A -> heap_clsf)
   (c: statement)
   (postP: A -> ret_assert) (postN: A -> stack_clsf) (postA: A -> heap_clsf)
@@ -84,97 +86,93 @@ Definition ifc_core {A: Type} (Delta: tycontext)
    let postP' := fun (x: A) => VST_post_to_state_pred (postP x) in
    simple_ifc Delta preP' preN preA c postP' postN postA.
 
-Definition proj13f {A T1 T2 T3: Type} (f: A -> (T1 * T2 * T3)): A -> T1 :=
-  fun (x: A) => match f x with
-  | (f, _, _) => f
-  end.
+Definition ifc_def {A: Set} {cs: compspecs} {Espec: OracleKind} (Delta: tycontext) 
+  (preP: A -> pre_assert) (preN: A -> stack_clsf) (preA: A -> heap_clsf)
+  (c: statement)
+  (postP: A -> ret_assert) (postN: A -> stack_clsf) (postA: A -> heap_clsf)
+:= (forall x: A, @semax cs Espec Delta (preP x) c (postP x)) /\
+   (ifc_core Delta preP preN preA c postP postN postA).
 
-Definition proj23f {A T1 T2 T3: Type} (f: A -> (T1 * T2 * T3)): A -> T2 :=
-  fun (x: A) => match f x with
-  | (_, f, _) => f
-  end.
-
-Definition proj33f {A T1 T2 T3: Type} (f: A -> (T1 * T2 * T3)): A -> T3 :=
-  fun (x: A) => match f x with
-  | (_, _, f) => f
-  end.
-
-Definition ifc_core0 {A: Type} (Delta: tycontext) (P: A -> ifc_pre) (c: statement) (Q: A -> ifc_post) :=
-  ifc_core Delta (proj13f P) (proj23f P) (proj33f P)
-                 c
-                 (proj13f Q) (proj23f Q) (proj33f Q).
-
-Definition semax0 {cs: compspecs} {Espec: OracleKind} (Delta: tycontext) 
-  (P: ifc_pre) (c: statement) (Q: ifc_post)
-:= semax Delta (fst (fst P)) c (fst (fst Q)).
-
-Definition ifc_def {A: Type} {cs: compspecs} {Espec: OracleKind} (Delta: tycontext) 
-  (P: A -> ifc_pre) (c: statement) (Q: A -> ifc_post)
-:= (forall x: A, semax0 Delta (P x) c (Q x)) /\
-   (ifc_core0 Delta P c Q).
-
-(*
-Notation "'ifc' [ x : A ] Delta |-- P c Q" :=
-  (@ifc_def A _ _ Delta (fun x => P) c (fun x => Q))
+Notation "'ifc' '[' x : A ']'  'semax' Delta preP preN preA c postP postN postA" :=
+  (@ifc_def A _ _ Delta (fun x => preP) (fun x => preN) (fun x => preA)
+                        c
+                        (fun x => postP) (fun x => postN) (fun x => postA))
   (at level 200,
-   x at level 0, A at level 0, Delta at level 0,
-   P at level 0, c at level 0, Q at level 0).
-*)
+   x at level 0, Delta at level 0,
+   preP at level 0, preN at level 0, preA at level 0,
+   c at level 0,
+   postP at level 0, postN at level 0, postA at level 0).
+
+Parameter D1: tycontext.
+Parameter P1: pre_assert.
+Parameter Q1: ret_assert.
+Definition N1: stack_clsf := fun i => if Pos.testbit i 0 then Lo else Hi.
+Definition A1: heap_clsf := fun l => let (b, ofs) := l in if Z.testbit ofs 0 then Lo else Hi.
+Require Import floyd.proofauto.
+Instance CompSpecs : compspecs. admit. Admitted.
+Instance Espec: OracleKind. Admitted.
+Definition t1 := ifc [ a : nat ] semax D1 P1 N1 A1 Sskip (fun _ => fun _ => !! (a = a)) N1 A1.
+Print t1.
 
 (* recursive binders only work for fun and forall, but not for match or let, so we have to write
    one definition for each arity *)
 
-Notation "'ifc' [ x1 : A1 , x2 : A2 ] Delta |-- P c Q" :=
-  (@ifc_def (A1*A2) _ _ Delta (fun x => match x with (x1,x2) => P end)
-                              c
-                              (fun x => match x with (x1,x2) => Q end))
+Notation "'ifc' '[' x1 : A1 , x2 : A2 ']'  'semax' Delta preP preN preA c postP postN postA" :=
+  (@ifc_def (A1*A2) _ _ Delta
+     (fun x => let (x1,x2) := x in preP)
+     (fun x => let (x1,x2) := x in preN)
+     (fun x => let (x1,x2) := x in preA)
+     c
+     (fun x => let (x1,x2) := x in postP)
+     (fun x => let (x1,x2) := x in postN)
+     (fun x => let (x1,x2) := x in postA))
 (at level 200,
  x1 at level 0, x2 at level 0,
- Delta at level 0, P at level 0, c at level 0, Q at level 0).
+ Delta at level 0,
+ preP at level 0, preN at level 0, preA at level 0,
+ c at level 0,
+ postP at level 0, postN at level 0, postA at level 0).
 
-Notation "'ifc' [ x1 : A1 , x2 : A2 , x3 : A3 ] Delta |-- P c Q" :=
+Definition t2 := ifc [a : nat, b: nat] semax D1 P1 N1 A1 Sskip (fun _ => fun _ => !! (a = b)) N1 A1.
+Print t2.
+
+Notation "'ifc' '[' x1 : A1 , x2 : A2 , x3 : A3 ']'  'semax' Delta preP preN preA c postP postN postA" :=
   (@ifc_def (A1*A2*A3) _ _ Delta
-            (fun x => match x with (x1,x2,x3) => P end)
-            c
-            (fun x => match x with (x1,x2,x3) => Q end))
+     (fun x => let (p2, x3) := x in let (x1,x2) := p2 in preP)
+     (fun x => let (p2, x3) := x in let (x1,x2) := p2 in preN)
+     (fun x => let (p2, x3) := x in let (x1,x2) := p2 in preA)
+     c
+     (fun x => let (p2, x3) := x in let (x1,x2) := p2 in postP)
+     (fun x => let (p2, x3) := x in let (x1,x2) := p2 in postN)
+     (fun x => let (p2, x3) := x in let (x1,x2) := p2 in postA))
 (at level 200,
  x1 at level 0, x2 at level 0, x3 at level 0,
- Delta at level 0, P at level 0, c at level 0, Q at level 0).
+ Delta at level 0,
+ preP at level 0, preN at level 0, preA at level 0,
+ c at level 0,
+ postP at level 0, postN at level 0, postA at level 0).
 
-Notation "'ifc' [ x1 : A1 , x2 : A2 , x3 : A3 , x4 : A4 ] Delta |-- P c Q" :=
+Definition t3 := ifc [a : nat, b: nat, c: nat] semax
+   D1 P1 N1 A1 Sskip (fun _ => fun _ => !! (a = b /\ b = c)) N1 A1.
+Print t3.
+
+Notation "'ifc' '[' x1 : A1 , x2 : A2 , x3 : A3 , x4 : A4 ']'  'semax' Delta preP preN preA c postP postN postA" :=
   (@ifc_def (A1*A2*A3*A4) _ _ Delta
-            (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1, x2) := p2 in P)
-            c
-            (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1, x2) := p2 in Q))
+     (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1,x2) := p2 in preP)
+     (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1,x2) := p2 in preN)
+     (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1,x2) := p2 in preA)
+     c
+     (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1,x2) := p2 in postP)
+     (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1,x2) := p2 in postN)
+     (fun x => let (p3, x4) := x in let (p2, x3) := p3 in let (x1,x2) := p2 in postA))
 (at level 200,
  x1 at level 0, x2 at level 0, x3 at level 0, x4 at level 0,
- Delta at level 0, P at level 0, c at level 0, Q at level 0).
+ Delta at level 0,
+ preP at level 0, preN at level 0, preA at level 0,
+ c at level 0,
+ postP at level 0, postN at level 0, postA at level 0).
 
-(*
-Notation "'ifc' [ x1 : A1 , x2 : A2 , x3 : A3 , x4 : A4 ] Delta |-- P c Q" :=
-  (@ifc_def (A1*A2*A3*A4) _ _ Delta
-            (fun x => match x with (x1,x2,x3,x4) => P end)
-            c
-            (fun x => match x with (x1,x2,x3,x4) => Q end))
-(at level 200,
- x1 at level 0, x2 at level 0, x3 at level 0, x4 at level 0,
- Delta at level 0, P at level 0, c at level 0, Q at level 0).
-*)
+Definition t4 := ifc [a : nat, b: nat, c: nat, d: int] semax
+   D1 P1 N1 A1 Sskip (fun _ => fun _ => !! (a = b /\ b = c /\ d = Int.zero)) N1 A1.
+Print t4.
 
-Notation "'ifc' [ x1 : A1 , x2 : A2 , x3 : A3 , x4 : A4 , x5 : A5 ] Delta |-- P c Q" :=
-  (@ifc_def (A1*A2*A3*A4*A5) _ _ Delta
-            (fun x => match x with (x1,x2,x3,x4,x5) => P end)
-            c
-            (fun x => match x with (x1,x2,x3,x4,x5) => Q end))
-(at level 200,
- x1 at level 0, x2 at level 0, x3 at level 0, x4 at level 0, x5 at level 0,
- Delta at level 0, P at level 0, c at level 0, Q at level 0).
-
-Notation "'ifc' [ x1 : A1 , x2 : A2 , x3 : A3 , x4 : A4 , x5 : A5 , x6 : A6 ] Delta |-- P c Q" :=
-  (@ifc_def (A1*A2*A3*A4*A5*A6) _ _ Delta
-            (fun x => match x with (x1,x2,x3,x4,x5,x6) => P end)
-            c
-            (fun x => match x with (x1,x2,x3,x4,x5,x6) => Q end))
-(at level 200,
- x1 at level 0, x2 at level 0, x3 at level 0, x4 at level 0, x5 at level 0, x6 at level 0,
- Delta at level 0, P at level 0, c at level 0, Q at level 0).
