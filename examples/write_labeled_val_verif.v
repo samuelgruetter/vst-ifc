0eeb366c20722c3a60337ef2f8a9b08189619133
Samuel Gruetter <samuel.gruetter@epfl.ch>
2017-05-04 22:00:04 +1000

    define ifc_funspec as a VST funspec + pre/post heap/stack classifications and define ifc_body similarly to semax_body, so that in the examples, VST has all the context it needs

--- examples/write_labeled_val_verif.v
+++ examples/write_labeled_val_verif.v
@@ -42,7 +42,74 @@ Record MyMetaVars: Type := { v: int; b: int; highptr: val; lowptr: val }.
 
 Set Printing Projections. (* to get x.(b) instead of (b x) in output *)
 
+Definition write_labeled_val_spec: ifc_funspec := {|
+  functional_spec :=
+    DECLARE _write_labeled_val
+    WITH x: MyMetaVars
+    PRE [ _v OF tint, _b OF tbool, _highptr OF (tptr tint), _lowptr OF (tptr tint) ]
+     (PROP (x.(b) = Int.zero \/ x.(b) = Int.one)
+      LOCAL (
+        temp _v (Vint x.(v)); temp _b (Vint x.(b)); temp _highptr x.(highptr); temp _lowptr x.(lowptr)
+      )
+      SEP (data_at_ Ews tint x.(highptr); data_at_ Ews tint x.(lowptr)))
+    POST [ tvoid ]
+      (PROP (x.(b) = Int.zero \/ x.(b) = Int.one)
+       LOCAL (
+         temp _v (Vint x.(v)); temp _b (Vint x.(b)); temp _highptr x.(highptr); temp _lowptr x.(lowptr)
+       )
+       SEP (data_at_ Ews tint x.(highptr); data_at_ Ews tint x.(lowptr)));
+  ifc_stack_pre :=
+    (fun (x: MyMetaVars) (i: ident) => PMap.get i 
+      (PMap.set _v (if Int.eq x.(b) Int.zero then Lo else Hi)
+      (PMap.set _b Lo
+      (PMap.set _highptr Hi
+      (PMap.set _lowptr Lo
+      (PMap.init Hi))))));
+  ifc_heap_pre :=
+    (fun (x: MyMetaVars) (loc: heap_loc) => Hi);
+  ifc_stack_post :=
+    (fun (x: MyMetaVars) (i: ident) => Hi);
+  ifc_heap_post :=
+    (fun (x: MyMetaVars) (loc: heap_loc) => Hi)
+|}.
+
+Definition Gprog: funspecs := [].
+
 Lemma verif_write_labeled_val:
+  ifc_body Vprog Gprog f_write_labeled_val write_labeled_val_spec.
+Proof.
+  unfold write_labeled_val_spec. hnf. intros. clear ts.
+  simpl (fn_body _).
+  (* TODO rule for Ssequence *)
+  replace (Ssequence
+   (Sifthenelse (Etempvar _b tbool)
+      (Sassign (Ederef (Etempvar _highptr (tptr tint)) tint)
+         (Etempvar _v tint))
+      (Sassign (Ederef (Etempvar _lowptr (tptr tint)) tint)
+         (Etempvar _v tint))) (Sreturn None))
+  with (Sifthenelse (Etempvar _b tbool)
+      (Sassign (Ederef (Etempvar _highptr (tptr tint)) tint)
+         (Etempvar _v tint))
+      (Sassign (Ederef (Etempvar _lowptr (tptr tint)) tint)
+         (Etempvar _v tint))) by admit.
+  apply ifc_ifthenelse; try reflexivity; unfold iand, iprop.
+  - (* then-branch *)
+    unfold ifc_def. split.
+    + (* VST part *)
+      intros. destruct x as [v b highptr lowptr] eqn: E.
+      match goal with
+      | _ : x = ?m |- _ => simpl (_ m)
+      end. clear E.
+      simplify_func_tycontext. abbreviate_semax.
+      (* forward. still fails *)
+      admit.
+    + (* IFC part *)
+      apply ifc_core0_always_holds.
+  - (* else-branch *)
+    admit.
+Admitted.
+
+Lemma verif_write_labeled_val_body_only:
   ifc [x: MyMetaVars] Delta |--
    (PROP (x.(b) = Int.zero \/ x.(b) = Int.one)
     LOCAL (
--- ifc/ifc.v
+++ ifc/ifc.v
@@ -118,3 +118,83 @@ Instance Espec: OracleKind. Admitted.
 Definition t1 := ifc [ a : nat ] D1 |-- P1 N1 A1 Sskip (fun _ => fun _ => !! (a = a)) N1 A1.
 Print t1.
 *)
+
+Definition WITHclause_type(fspec: ident * funspec): Type := match fspec with
+| (_, mk_funspec _ _ (rmaps.ConstType A) _ _ _ _) => A
+(* error: we always expect an rmaps.ConstType, because that's what NDmk_funspec passes to mk_funspec *)
+| _ => unit
+end.
+
+Record ifc_funspec: Type := mk_ifc_funspec {
+  functional_spec : (ident * funspec)%type;
+  ifc_stack_pre   : (WITHclause_type functional_spec) -> stack_clsf;
+  ifc_heap_pre    : (WITHclause_type functional_spec) -> heap_clsf;
+  ifc_stack_post  : (WITHclause_type functional_spec) -> stack_clsf;
+  ifc_heap_post   : (WITHclause_type functional_spec) -> heap_clsf
+}.
+
+Local Open Scope logic.
+
+Require Import veric.rmaps.
+
+(* "T -> assertion" *)
+Definition vst_fun_assert (T: Type): Type :=
+  forall ts : list Type,
+    msl.functors.MixVariantFunctor._functor 
+     (dependent_type_functor_rec ts (AssertTT (rmaps.ConstType T))) mpred.
+
+(* inspired by semax_body in VST/veric/semax_prog.v *)
+Definition ifc_body00 (V: varspecs) (G: funspecs) (C: compspecs) (f: function)
+  (T: Type)
+  (P : vst_fun_assert T) (N : T -> stack_clsf) (A : T -> heap_clsf)
+  (P': vst_fun_assert T) (N': T -> stack_clsf) (A': T -> heap_clsf)
+: Prop :=
+  forall Espec ts,
+    @ifc_def T C Espec (func_tycontext f V G)
+             (fun x rho => P ts x rho * stackframe_of f rho)
+             N A
+             (Ssequence f.(fn_body) (Sreturn None))
+             (fun x => (frame_ret_assert (function_body_ret_assert (fn_return f) (P' ts x))
+                                         (stackframe_of f)))
+             N' A'.
+
+Definition force_const_type(t: TypeTree): Type :=
+      match t with
+      | ConstType A0 => A0
+      | Mpred => unit
+      | DependentType _ => unit
+      | ProdType _ _ => unit
+      | ArrowType _ _ => unit
+      end.
+
+(* dependent types craziness... *)
+Definition ifc_body0 (V : varspecs) (G : funspecs) (C : compspecs) (f : function) (i : ident)
+  (fsig : base.funsig) (c : calling_convention) (T0 : TypeTree)
+  (P P' : forall ts : list Type,
+        functors.MixVariantFunctor._functor
+          (dependent_type_functor_rec ts (AssertTT T0)) mpred)
+  (P_ne : super_non_expansive P) (Q_ne : super_non_expansive P')
+  (N  : WITHclause_type (i, mk_funspec fsig c T0 P P' P_ne Q_ne) -> stack_clsf)
+  (A  : WITHclause_type (i, mk_funspec fsig c T0 P P' P_ne Q_ne) -> heap_clsf)
+  (N' : WITHclause_type (i, mk_funspec fsig c T0 P P' P_ne Q_ne) -> stack_clsf)
+  (A' : WITHclause_type (i, mk_funspec fsig c T0 P P' P_ne Q_ne) -> heap_clsf)
+: Prop := 
+  match T0 as t return
+    (let T := force_const_type t in (forall
+       P0 P'0 ,
+     super_non_expansive P0 ->
+     super_non_expansive P'0 ->
+     (T -> stack_clsf) ->
+     (T -> heap_clsf) ->
+     (T -> stack_clsf) ->
+     (T -> heap_clsf) -> Prop))
+  with
+  | ConstType T => fun P0 P'0 _ _ N0 A0 N'0 A'0 => ifc_body00 V G C f T P0 N0 A0 P'0 N'0 A'0
+  | _ => fun _ _ _ _ _ _ _ _  => False
+  end P P' P_ne Q_ne N A N' A'.
+
+Definition ifc_body
+       (V: varspecs) (G: funspecs) {C: compspecs} (f: function) (spec: ifc_funspec) : Prop :=
+  match spec with (mk_ifc_funspec (i, mk_funspec fsig cc T P P' Pne P'ne) N A N' A') =>
+    ifc_body0 V G C f i fsig cc T P P' Pne P'ne N A N' A'
+  end.
