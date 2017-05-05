d03bf0bdda26dbccbb760d183ebc95410149e32e
Samuel Gruetter <samuel.gruetter@epfl.ch>
2017-05-05 18:57:23 +1000

    change ifc_def so that it only talks about the final state of terminating executions

--- ifc/ifc.v
+++ ifc/ifc.v
@@ -34,13 +34,13 @@ Definition ifc_post(A: Type) := ((A -> ret_assert) * (A -> stack_clsf) * (A -> h
 Parameter VST_pre_to_state_pred : pre_assert -> state_pred.
 Parameter VST_post_to_state_pred : ret_assert -> state_pred.
 
-Inductive star (ge: genv): corestate -> mem -> nat -> corestate -> mem -> Prop :=
+Inductive star (ge: genv): corestate -> mem -> corestate -> mem -> Prop :=
   | star_refl: forall s m,
-      star ge s m O s m
-  | star_step: forall n s1 m1 s2 m2 s3 m3,
+      star ge s m s m
+  | star_step: forall s1 m1 s2 m2 s3 m3,
       cl_step ge s1 m1 s2 m2 ->
-      star ge s2 m2 n s3 m3 ->
-      star ge s1 m1 (S n) s3 m3.
+      star ge s2 m2 s3 m3 ->
+      star ge s1 m1 s3 m3.
 
 (* general low-equivalence *)
 Definition gen_lo_equiv{Loc V: Type}(f1 f2: Loc -> label)(s1 s2: Loc -> V) :=
@@ -64,17 +64,19 @@ Definition simple_ifc {A : Type} (Delta: tycontext)
   (preP: A -> state_pred) (preN: A -> stack_clsf) (preA: A -> heap_clsf)
   (c: statement)
   (postP: A -> state_pred) (postN: A -> stack_clsf) (postA: A -> heap_clsf)
-:= forall (x1 x2: A) (ge: genv) (e1 e2: env) (te1 te2: temp_env) (s1' s2': corestate)
-          (m1 m1' m2 m2': mem) (n: nat),
-   let s1 := (State e1 te1 [Kseq c]) in
-   let s2 := (State e2 te2 [Kseq c]) in
-   preP x1 s1 m1 ->
-   preP x2 s2 m2 ->
-   stack_lo_equiv s1 s2 (preN x1) (preN x2) ->
-   heap_lo_equiv  m1 m2 (preA x1) (preA x2) ->
-   star ge s1 m1 n s1' m1' ->
-   star ge s2 m2 n s2' m2' ->
-   stack_lo_equiv s1' s2' (postN x1) (postN x2) /\ heap_lo_equiv m1 m2 (postA x1) (postA x2).
+:= forall (x x': A) (ge: genv) (e1 e1' e2 e2': env) (te1 te1' te2 te2': temp_env)
+          (m1 m1' m2 m2': mem),
+   let s1  := (State e1  te1  [Kseq c]) in
+   let s1' := (State e1' te1' [Kseq c]) in
+   let s2  := (State e2  te2  nil) in
+   let s2' := (State e2' te2' nil) in
+   preP x  s1  m1 ->
+   preP x' s1' m1' ->
+   stack_lo_equiv s1 s1' (preN x) (preN x') ->
+   heap_lo_equiv m1 m1' (preA x) (preA x') ->
+   star ge s1  m1  s2  m2 ->
+   star ge s1' m1' s2' m2' ->
+   stack_lo_equiv s2 s2' (postN x) (postN x') /\ heap_lo_equiv m2 m2' (postA x) (postA x').
 
 Definition ifc_core {A: Type} (Delta: tycontext)
   (preP: A -> pre_assert) (preN: A -> stack_clsf) (preA: A -> heap_clsf)
